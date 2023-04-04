65a66d4c608d18069093940576891d75bfb0d487
manikanta.kg <manikanta.kg@xelpmoc.in>
2023-04-04 11:20:10 +0530

    point-precison is added

--- main.go
+++ main.go
@@ -27,5 +27,6 @@ func main() {
 	// str.GeneralVerb()
 	// str.Index()
 	// str.FormatConversion()
-	str.FormatInteger()
+	// str.FormatInteger()
+	str.PointPrecison()
 }
---
+++ str/pointPrecision.go
@@ -0,0 +1,12 @@
+package str
+
+import "fmt"
+
+func PointPrecison() {
+
+	// Observe the Point-Precison
+	fmt.Printf("%0.f\n", 16.540)
+	fmt.Printf("%0.2f\n", 16.540)
+	fmt.Printf("%0.3f\n", 16.540)
+	fmt.Printf("%0.5f\n", 16.540)
+}
