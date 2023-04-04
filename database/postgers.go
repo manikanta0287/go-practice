84a0af44e7d6b4f33ed19011e3cc692cd414e9ba
manikanta.kg <manikanta.kg@xelpmoc.in>
2023-04-04 18:11:57 +0530

    postgres added

--- database/postgers.go
+++ database/postgers.go
@@ -1,2 +1,17 @@
 package database
 
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func DataConnection() {
+	dsn := "host=localhost user=root password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Postgres DB connected", db)
+}
