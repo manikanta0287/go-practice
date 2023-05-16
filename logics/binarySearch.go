f6bc068cd4158ad44efacf678577786975517921
manikanta.kg <manikanta.kg@xelpmoc.in>
2023-05-16 11:23:09 +0530

    minor changes in binary search

--- logics/binarySearch.go
+++ logics/binarySearch.go
@@ -1,21 +1,21 @@
 package logics
 
-func BinarySearch(needle int, haystack []int) bool {
+func BinarySearch(needle int, arr []int) bool {
 
 	low := 0
-	high := len(haystack) - 1
+	high := len(arr) - 1
 
 	for low <= high {
 		median := (low + high) / 2
 
-		if haystack[median] < needle {
+		if arr[median] < needle {
 			low = median + 1
 		} else {
 			high = median - 1
 		}
 	}
 
-	if low == len(haystack) || haystack[low] != needle {
+	if low == len(arr) || arr[low] != needle {
 		return false
 	}
 
